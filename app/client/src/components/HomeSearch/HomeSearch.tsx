b83a90444f9b3add7e0c5322605646a75dc73f6c
Deepraj <deepraj21.bera@gmail.com>
2025-01-22 11:17:02 +0530

    fix: adjust margin for HomeMarquee component in HomeSearch for improved layout

--- app/client/src/components/HomeSearch/HomeSearch.tsx
+++ app/client/src/components/HomeSearch/HomeSearch.tsx
@@ -299,10 +299,9 @@ const HomeSearch = () => {
                                             <h1 className="md:text-4xl text-2xl font-normal text-center mb-6">
                                                 What can I help with?
                                             </h1>
-                                            <div className="w-full">
+                                            <div className="w-full ml-4 md:ml-0">
                                                 <HomeMarquee setSearchQuery={setSearchQuery} />
                                             </div>
-
                                         </div>
                                     )
                                 }
